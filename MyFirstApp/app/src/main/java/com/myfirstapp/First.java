8328ae5e7d7aa5f71e22f5da25a1498fe7a006b2
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2024-05-31 19:16:20 +0545

    JAVA Class and Other File Created and Added to the AndroidManifest.xml

---
+++ MyFirstApp/app/src/main/java/com/myfirstapp/First.java
@@ -0,0 +1,14 @@
+package com.myfirstapp;
+import android.app.Activity;
+import android.os.Bundle;
+import android.text.Layout;
+
+public class First extends Activity {
+    @Override
+    protected void onCreate(Bundle b){
+        super.onCreate(b);
+        setContentView(R.layout.first);
+    }
+
+
+}
