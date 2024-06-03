edf469c06d33433cecd774d1893daff02dd1464c
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2024-06-03 14:40:17 +0545

    Initial

---
+++ MyFirstApp/app/src/main/java/com/myfirstapp/Second.java
@@ -0,0 +1,12 @@
+package com.myfirstapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Second extends Activity {
+    protected void onCreate(Bundle b){
+        super.onCreate(b);
+        setContentView(R.layout.second);
+    }
+
+}
