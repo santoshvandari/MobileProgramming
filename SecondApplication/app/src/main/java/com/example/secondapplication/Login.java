eb2c43cd7a6a3dad6e6d77a2d2910987dd921167
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2024-06-03 15:14:31 +0545

    Initial

---
+++ SecondApplication/app/src/main/java/com/example/secondapplication/Login.java
@@ -0,0 +1,12 @@
+package com.example.secondapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Login extends Activity {
+    @Override
+    protected void onCreate(Bundle b){
+        super.onCreate(b);
+        setContentView(R.layout.login);
+    }
+}
