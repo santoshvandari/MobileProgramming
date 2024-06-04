ababfd4e5e9a4363e2052ea5e3c1b4617caaf841
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2024-06-04 14:41:25 +0545

    Update

---
+++ SecondApplication/app/src/main/java/com/example/secondapplication/Distribution.java
@@ -0,0 +1,11 @@
+package com.example.secondapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Distribution extends Activity {
+    protected void onCreate(Bundle b){
+        super.onCreate(b);
+        setContentView(R.layout.distribution);
+    }
+}
