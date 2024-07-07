0f5c669c54b54b1e44af49d1650bc9d38855b719
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2024-07-07 17:22:24 +0545

    INitial

--- Labsheet/FourthApplication/app/src/main/java/com/example/fourthapplication/SumOfNumbers.java
+++ Labsheet/FourthApplication/app/src/main/java/com/example/fourthapplication/SumOfNumbers.java
@@ -1,4 +1,28 @@
 package com.example.fourthapplication;
 
-public class SumOfNumbers {
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class SumOfNumbers extends Fragment {
+    public View onCreateView(LayoutInflater li, ViewGroup vg, Bundle b){
+        super.onCreateView(li,vg,b);
+        View view = li.inflate(R.layout.sumofnumbers,vg,false);
+
+        EditText num1=view.findViewById(R.id.num1);
+        EditText num2=view.findViewById(R.id.num2);
+        Button btn = view.findViewById(R.id.calc);
+        TextView txt = view.findViewById(R.id.res);
+        btn.setOnClickListener(e->{
+            int n1=Integer.parseInt(num1.toString());
+            int n2=Integer.parseInt(num2.toString());
+            txt.setText(String.valueOf(n1+n2));
+        });
+        return view;
+    }
 }
