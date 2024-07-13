11fd93f8b0320e51948e55cca1975b020026449a
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2024-07-13 09:02:21 +0545

    Smple Interest

---
+++ Labsheet/Lab1/app/src/main/java/com/example/lab1/SimpleInterest.java
@@ -0,0 +1,24 @@
+package com.example.lab1;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class SimpleInterest extends Activity {
+    protected void  onCreate(Bundle b){
+        super.onCreate(b);
+        setContentView(R.layout.simpleinterest);
+        EditText pri=findViewById(R.id.principal);
+        EditText rate = findViewById(R.id.rate);
+        EditText time = findViewById(R.id.time);
+        TextView interest=findViewById(R.id.interst);
+        findViewById(R.id.calc).setOnClickListener(e->{
+            int p=Integer.parseInt(pri.getText().toString());
+            int r=Integer.parseInt(rate.getText().toString());
+            int t=Integer.parseInt(time.getText().toString());
+            interest.setText("Interst : "+(p*t*r/100));
+        });
+
+    }
+}
