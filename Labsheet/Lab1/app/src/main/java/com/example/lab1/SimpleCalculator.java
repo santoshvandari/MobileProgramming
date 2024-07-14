0f288fefb122615cd418960cdd45c91800557f16
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2024-07-14 07:13:18 +0545

    initial

--- Labsheet/Lab1/app/src/main/java/com/example/lab1/SimpleCalculator.java
+++ Labsheet/Lab1/app/src/main/java/com/example/lab1/SimpleCalculator.java
@@ -1,6 +1,19 @@
 package com.example.lab1;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class SimpleCalculator extends Activity {
+    protected void onCreate(Bundle b){
+        super.onCreate(b);
+        setContentView(R.layout.simplecalculator);
+        EditText num1 = findViewById(R.id.num1);
+        EditText num2 = findViewById(R.id.num2);
+        Button add = findViewById(R.id.add);
+        Button sub = findViewById(R.id.sub);
+        Button mul = findViewById(R.id.mul);
+        Button div = findViewById(R.id.div);
+    }
 }
