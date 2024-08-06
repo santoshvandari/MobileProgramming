2e06c4714fd38170d9f9b58ff928151e9fde2ae9
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2024-08-06 17:22:18 +0545

    Updated

--- SixthSemester/app/src/main/java/com/example/sixthsemester/DBForm.java
+++ SixthSemester/app/src/main/java/com/example/sixthsemester/DBForm.java
@@ -1,8 +1,10 @@
 package com.example.sixthsemester;
 
 import android.app.Activity;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class DBForm extends Activity {
@@ -13,13 +15,47 @@ public class DBForm extends Activity {
         EditText id = findViewById(R.id.sid);
         EditText name = findViewById(R.id.name);
         EditText address = findViewById(R.id.address);
-        findViewById(R.id.submit).setOnClickListener(e->{
+        TextView txtres = findViewById(R.id.res);
+
+        findViewById(R.id.insert).setOnClickListener(e->{
             int sid = Integer.parseInt(id.getText().toString());
             String sname=name.getText().toString();
-            String saddress = name.getText().toString();
+            String saddress = address.getText().toString();
             helper.Insert(sid,sname,saddress);
             Toast.makeText(this,"Data Inserted Successfully!",Toast.LENGTH_SHORT).show();
 
+        });
+
+        findViewById(R.id.update).setOnClickListener(e->{
+            String sid =id.getText().toString();
+            String sname=name.getText().toString();
+            String saddress = address.getText().toString();
+            helper.Update(sid,sname,saddress);
+            Toast.makeText(this,"Data Updated Successfully!",Toast.LENGTH_SHORT).show();
+
+
+        });
+
+
+        findViewById(R.id.delete).setOnClickListener(e->{
+            String sid = id.getText().toString();
+            helper.Delete(sid);
+            Toast.makeText(this,"Data Deleted Successfully!",Toast.LENGTH_SHORT).show();
+
+
+        });
+
+
+        findViewById(R.id.read).setOnClickListener(e->{
+            Cursor cursor = helper.Select();
+            String res = "Sid\tName\tAddress\n";
+            while(cursor.moveToNext()){
+                int sid = cursor.getInt(0);
+                String sname = cursor.getString(1);
+                String sadd = cursor.getString(2);
+                res+=sid+"\t"+sname+"\t"+sadd+"\n";
+            }
+            txtres.setText(res);
         });
 
     }
--- SixthSemester/app/src/main/java/com/example/sixthsemester/MyDBHelper.java
+++ SixthSemester/app/src/main/java/com/example/sixthsemester/MyDBHelper.java
@@ -17,18 +17,50 @@ public class MyDBHelper extends SQLiteOpenHelper {
 //        Used to Upgrade Database;
     }
 
-    public void Insert(int sid, String name, String address){
+    public void Insert(String sid, String name, String address){
         SQLiteDatabase db = this.getWritableDatabase();
-        ContentValues values = new ContentValues();
-        values.put("sid",sid);
-        values.put("name",name);
-        values.put("address",address);
-        db.insert("Student",null,values);
-        db.close();
-      /*  String sql = "INSERT INTO Student(sid,name,address) VALUES("+sid+",'"+name+"','"+address+"');";
+//        ContentValues values = new ContentValues();
+//        values.put("sid",sid);
+//        values.put("name",name);
+//        values.put("address",address);
+//        db.insert("Student",null,values);
+//        db.close();
+//      /*
+      String sql = "INSERT INTO Student(sid,name,address) VALUES("+sid+",'"+name+"','"+address+"');";
         db.execSQL(sql);
-        db.close();     */
+        db.close();
+//         */
     }
 
+    public void Update(String sid, String name, String address){
+        SQLiteDatabase db = this.getWritableDatabase();
+//        ContentValues cv = new ContentValues();
+//        cv.put("name",name);
+//        cv.put("address",address);
+//        db.update("Student",cv,"sid=?",new String[]{sid});
+//        db.close();
+
+//        /*
+        String sql = "UPDATE ";
+        db.execSQL(sql);
+        db.close();
+
+//         */
+    }
+
+    public void Delete(String sid){
+        SQLiteDatabase db = this.getWritableDatabase();
+//        db.delete("Student","sid=?",new String[]{sid});
+//        db.close();
+
+
+//        /*
+        String sql = "DELETE FROM Student WHERE sid = "+sid+";";
+        db.execSQL(sql);
+        db.close();
+
+
+//         */
+    }
 
 }
