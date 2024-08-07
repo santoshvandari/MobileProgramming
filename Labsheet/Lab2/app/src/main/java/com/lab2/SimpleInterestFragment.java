766a4d693453ee96e42f10d07bebbb39072c7eaf
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2024-08-07 11:30:45 +0545

    Initial

--- Labsheet/Lab2/app/src/main/java/com/lab2/SimpleInterestFragment.java
+++ Labsheet/Lab2/app/src/main/java/com/lab2/SimpleInterestFragment.java
@@ -5,10 +5,19 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class SimpleInterestFragment extends Fragment {
     public View onCreateView(LayoutInflater li, ViewGroup vg, Bundle b){
         View view = li.inflate(R.layout.simpleinterestfragment,vg,false);
+        EditText pri  = view.findViewById(R.id.principle);
+        EditText rate = view.findViewById(R.id.rate);
+        EditText time = view.findViewById(R.id.time);
+        TextView res = view.findViewById(R.id.res);
+        view.findViewById(R.id.calc).setOnClickListener(e->{
+
+        });
         return view;
     }
 }
