6806dc07e3b4363b53f4b30dd9c666e5dbcccf05
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2024-08-15 19:59:15 +0545

    Updated

--- Practice/app/src/main/java/com/practice/CustomDialogActivity.java
+++ Practice/app/src/main/java/com/practice/CustomDialogActivity.java
@@ -1,6 +1,33 @@
 package com.practice;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class CustomDialogActivity extends Activity {
+    protected void onCreate(Bundle b){
+        super.onCreate(b);
+        setContentView(R.layout.customdialogactivity);
+        findViewById(R.id.calcsi).setOnClickListener(e->{
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            View view = getLayoutInflater().inflate(R.layout.customdialogsimpleinterest,null);
+            builder.setView(view);
+            EditText pri = view.findViewById(R.id.principal);
+            EditText rate = view.findViewById(R.id.rate);
+            EditText time = view.findViewById(R.id.time);
+            TextView res = view.findViewById(R.id.res);
+            view.findViewById(R.id.calc).setOnClickListener(z->{
+                int p = Integer.parseInt(pri.getText().toString());
+                int t = Integer.parseInt(rate.getText().toString());
+                int r = Integer.parseInt(time.getText().toString());
+                int si = p*t*r/100;
+                res.setText("Interst : "+si);
+            });
+            builder.create().show();
+        });
+    }
+
 }
